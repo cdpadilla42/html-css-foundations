4fb2fcdda6ffb6f3d5588c69197d29b6c444834e
Chris Padilla <cdpadilla42@gmail.com>
2020-06-12 15:30:58 -0500

    Debug modal removal

--- modal/TOP.js
+++ modal/TOP.js
@@ -1,24 +1,33 @@
 const modal = document.querySelector('.modal');
 const hi = document.querySelector('.body__text');
 const closeBtn = document.querySelector('.modal span');
-const modalBackdrop = document.createElement('div');
-modalBackdrop.classList.add('modal__backdrop');
 
 const toggleModal = () => {
   modal.classList.toggle('hide');
 };
 
 const openModal = () => {
-  hi.appendChild(modalBackdrop);
-  modalBackdrop.addEventListener('click', closeModal);
+  let modalBackdrop = document.querySelector('.modal__backdrop');
+  modalBackdrop.classList.remove('hide');
   toggleModal();
 };
 
 const closeModal = () => {
-  let modalBackdrop = document.querySelector('.modal__backdrop');
-  hi.removeChild(modalBackdrop);
+  removeBackdrop();
   toggleModal();
+  console.log('close');
+};
+
+const removeBackdrop = () => {
+  let modalBackdrop = document.querySelector('.modal__backdrop');
+  modalBackdrop.classList.add('hide');
 };
 
 hi.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
+
+document.addEventListener('click', (e) => {
+  if (e.target == document.querySelector('.modal__backdrop')) {
+    closeModal();
+  }
+});
