c1f8e1031eb30ec8005f70aa8cff98cb75bae6f4
Chris Padilla <cdpadilla42@gmail.com>
2020-06-25 11:27:54 -0500

    Add js to keep email address floating once input is put into the form

--- footers/netlify.js
+++ footers/netlify.js
@@ -1,10 +1,13 @@
 const emailInput = document.querySelector('.newsletter__form #email');
 const emailLabel = document.querySelector('.newsletter__form #email_label');
+
 function handleFocus(e) {
   emailLabel.classList.add('focused');
 }
 function handleFocusOut(e) {
-  emailLabel.classList.remove('focused');
+  if (!emailInput.value.length) {
+    emailLabel.classList.remove('focused');
+  }
 }
 
 emailInput.addEventListener('focus', handleFocus);
